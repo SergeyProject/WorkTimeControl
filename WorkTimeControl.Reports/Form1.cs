316cc8be5b3d7ff7203be6556a1af771bc36ef36
Сергей М <msa1024@hotmail.com>
2021-09-30 21:31:57 +0300

    Removing an extra method

--- WorkTimeControl.Reports/Form1.cs
+++ WorkTimeControl.Reports/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WorkTimeControl.Client.Camera;
 using WorkTimeControl.DATA.Models;
@@ -14,27 +8,14 @@ using WorkTimeControl.DATA.Repositories;
 namespace WorkTimeControl.Reports
 {
     public partial class Form1 : Form
-    {
-        List<UserTime> listUserTime = new List<UserTime>();
-        //List<DateTime> listTime = new List<DateTime>();
-        //ListViewItem viewItem = null;
+    {       
         List<byte[]> listImgStart = new List<byte[]>();
         List<byte[]> listImgStop = new List<byte[]>();
-        //List<int> _idx = new List<int>();
-        byte[] emptyImg = { 1, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0 };
-
         List<TimeUser> listTimeUser = new List<TimeUser>(); 
-
         public Form1()
         {
             InitializeComponent();
-            listView1.SelectedIndexChanged += ListView1_SelectedIndexChanged;
-            dateTimePicker1.ValueChanged += DateTimePicker1_ValueChanged;
-        }
-
-        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-          
+            listView1.SelectedIndexChanged += ListView1_SelectedIndexChanged;           
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -45,65 +26,16 @@ namespace WorkTimeControl.Reports
             listImgStop.Clear();
 
             UserRepository db = new UserRepository();
-            listView1.Items.Clear();
-            //listUserTime.Clear();
+            listView1.Items.Clear();          
             listTimeUser.Clear();
 
 
             foreach (User user in db.GetAllUsers())
-            {
-                // LoadListView(user.Id);            /////////////////////////////
+            {               
                 LoadListTimeUser(user.Id);
             }
-
             loadViewLst();
-        }      
-
-        private void LoadListView(int userID)
-        {
-            try
-            {
-                ListViewItem viewItem = null;
-                UserTimeRepository db = new UserTimeRepository();
-                UserRepository userDb = new UserRepository();
-               
-                string[] value = new string[2];
-
-                foreach (UserTime userTime in db.GetAllUserTime())
-                {
-                    if (userTime.DateTimes.Date == dateTimePicker1.Value.Date)
-                    {
-                        if (userTime.UserId == userID)
-                        {                           
-                            viewItem = new ListViewItem(userTime.DateTimes.Date.ToShortDateString());
-                           
-                            viewItem.SubItems.Add(userDb.GetUserById(userTime.UserId).Name);
-                            if (userTime.Descript == "Приход на работу")
-                            {
-                                value[0] = userTime.DateTimes.ToLongTimeString();
-                                listImgStart.Add(userTime.Photo);
-                             
-                            }
-                            if (userTime.Descript == "Уход с работы")
-                            {
-                                value[1] = userTime.DateTimes.ToLongTimeString();
-                                listImgStop.Add(userTime.Photo);                              
-                            }   
-                        }
-                    }
-                }
-                if (viewItem != null)
-                {
-                    viewItem.SubItems.Add(value[0]);
-                    viewItem.SubItems.Add(value[1]);
-                    listView1.Items.Add(viewItem);                   
-                }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+        }     
 
         private void LoadListTimeUser(int userID)
         {
@@ -134,7 +66,6 @@ namespace WorkTimeControl.Reports
                             value[1] = userTime.DateTimes.ToShortTimeString();                           
                             imgStop = userTime.Photo;
                         }
-
                     }
                 }
             }
